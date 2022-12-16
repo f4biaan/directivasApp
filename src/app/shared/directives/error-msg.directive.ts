8a87f4512d6f9857c9ca5659f026b862a7e708ad
Fabián Montoya <jose.fabian.montoya9@gmail.com>
2022-12-16 16:15:18 -0500

    Cambiar mensaje de etiqueta

--- src/app/shared/directives/error-msg.directive.ts
+++ src/app/shared/directives/error-msg.directive.ts
@@ -7,6 +7,7 @@ export class ErrorMsgDirective implements OnInit {
 
   htmlElement: ElementRef<HTMLElement>;
   @Input() color: string = 'red';
+  @Input() mensaje: string = 'Campo requerido';
 
   constructor(private el: ElementRef<HTMLElement>) {
     console.log('Constructor de la directiva');
@@ -18,9 +19,15 @@ export class ErrorMsgDirective implements OnInit {
   ngOnInit(): void {
     console.log('ngOnInit de la directiva');
     this.setColor();
+    this.setMensaje();
   }
 
   setColor(): void {
     this.htmlElement.nativeElement.style.color = this.color;
+    this.htmlElement.nativeElement.classList.add('form-text');
+  }
+
+  setMensaje(): void {
+    this.htmlElement.nativeElement.innerText = this.mensaje;
   }
 }
