7b42ccf7a3fb623c67f691d779e757497ac8430f
Fabián Montoya <jose.fabian.montoya9@gmail.com>
2022-12-16 16:45:03 -0500

    getters en input

--- src/app/productos/pages/agregar/agregar.component.ts
+++ src/app/productos/pages/agregar/agregar.component.ts
@@ -9,6 +9,9 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class AgregarComponent {
 
+  texto1: string = 'f4biaan';
+  color: string = 'red';
+
   miFormulario: FormGroup = this.fb.group({
     nombre: ['', [Validators.required]],
   });
@@ -17,6 +20,15 @@ export class AgregarComponent {
 
   tieneError(campo: string) {
     return this.miFormulario.get(campo)?.invalid || false;
+  }
+
+  cambiarNombre() {
+    this.texto1 = Math.random().toString();
+  }
+
+  cambiarColor() {
+    const color = '#xxxxxx'.replace(/x/g, y=>(Math.random()*16|0).toString(16));
+    this.color = color;
   }
 
 }
--- src/app/shared/directives/error-msg.directive.ts
+++ src/app/shared/directives/error-msg.directive.ts
@@ -1,13 +1,33 @@
-import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 
 @Directive({
   selector: '[error-msg]',
 })
-export class ErrorMsgDirective implements OnInit {
+export class ErrorMsgDirective implements OnInit, OnChanges {
+
+  private _mensaje: string = 'Campo requerido';
+  private _color: string = 'red';
 
   htmlElement: ElementRef<HTMLElement>;
-  @Input() color: string = 'red';
-  @Input() mensaje: string = 'Campo requerido';
+
+  @Input() set color(valor: string) {
+    // this.htmlElement.nativeElement.style.color = valor;
+    this._color = valor;
+    this.setColor();
+  }
+  // @Input() mensaje: string = 'Campo requerido';
+  @Input() set mensaje (valor: string) {
+    // this.htmlElement.nativeElement.innerText = valor;
+    this._mensaje = valor;
+    this.setMensaje();
+  }
 
   constructor(private el: ElementRef<HTMLElement>) {
     console.log('Constructor de la directiva');
@@ -17,17 +37,28 @@ export class ErrorMsgDirective implements OnInit {
   }
 
   ngOnInit(): void {
-    console.log('ngOnInit de la directiva');
+    // console.log('ngOnInit de la directiva');
+    // console.log(this.color); // undefined
+    // console.log(this.mensaje); // undefined
+    this.setEstilo();
     this.setColor();
     this.setMensaje();
   }
 
-  setColor(): void {
-    this.htmlElement.nativeElement.style.color = this.color;
+  ngOnChanges(changes: SimpleChanges): void {
+    // console.log(changes);
+    // no es eficiente para hace de forma dinamica el cambio de propiedades segun los cambios que se realicen
+  }
+
+  setEstilo(): void {
     this.htmlElement.nativeElement.classList.add('form-text');
   }
 
+  setColor(): void {
+    this.htmlElement.nativeElement.style.color = this._color;
+  }
+
   setMensaje(): void {
-    this.htmlElement.nativeElement.innerText = this.mensaje;
+    this.htmlElement.nativeElement.innerText = this._mensaje;
   }
 }
